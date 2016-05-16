45710e46967a6113e2435cebbac0a13dd2a5ec90
Fred Lee <fredslee27@gmail.com>
2016-05-15 21:50:14 -0700

    Added PathComponent converters.

--- src/filst.c
+++ src/filst.c
@@ -4,7 +4,27 @@
 #include <filst.h>
 
 
+#include "conv.c"
 
+
+
+
+/* malloc to accomodate a given Component Identifier length. */
+struct path_component_s *
+path_component_malloc (int cilen)
+{
+  struct path_component_s * retval;
+
+  int msize = sizeof(struct path_component_s) + cilen + 1;
+  /* Plus 1 for terminating NUL; eases use with str*(). */
+  retval = (struct path_component_s *)malloc(msize);
+  memset(retval, 0, msize);
+  retval->len = cilen;
+  return retval;
+}
+
+
+/* Populate instance from parameters. */
 struct path_component_s *
 path_component_init (struct path_component_s * obj,
                      enum path_component_type_e pctype,
@@ -13,17 +33,95 @@ path_component_init (struct path_component_s * obj,
                      char ci[])
 {
   if (! obj)
+    obj = path_component_malloc(cilen);
+  if (! obj)
     {
-      obj = malloc(sizeof(struct path_component_s) + cilen);
+      abort();
     }
 
-  memset(obj, 0, sizeof(*obj));
-
   obj->typ = pctype;
+  obj->vers = version;
+  /* Bounds-check is far too primitive; either assumes the object passed in is
+   * already large enough, or was allocated in here for exactly the right size.
+   */
   obj->len = cilen;
 //  obj->ci = ci;
   memcpy(obj->d, ci, obj->len);
   return obj;
 }
+
+
+/* Create read-only instance (on heap) from parameters. */
+const struct path_component_s *
+path_component_make (enum path_component_type_e pctype,
+                     unsigned int version,
+                     int cilen,
+                     char ci[])
+{
+  if (cilen < 0)
+    cilen = strlen(ci);
+  struct path_component_s * obj = path_component_malloc(cilen);
+
+  obj->raw = NULL;
+  obj->typ = pctype;
+  obj->vers = version;
+  memcpy(obj->d, ci, cilen);
+  obj->d[cilen] = '\0';
+
+  return obj;
+}
+
+
+void
+path_component_dump (const struct path_component_s * obj)
+{
+  printf("path_component(@%p) = {\n", obj);
+  printf("  ct = %d\n", obj->typ);
+  printf("  cfvn = %d\n", obj->vers);
+  printf("  L_CI = %d\n", obj->len);
+  if (obj->len)
+    printf("  ci = \"%s\"\n", obj->d);
+  else
+    puts("  ci = \"\"");
+  puts("}");
+}
+
+
+const struct path_component_s *
+path_component_decode (void * space)
+{
+  struct path_component_s * obj;
+  layoutvalue_t contents[5] = { 0, };
+
+  udf_decode(space, udf_path_component, contents);
+
+  obj = path_component_malloc(contents[1].word);
+  obj->raw = space;
+  obj->typ = contents[0].word;
+  obj->len = contents[1].word;
+  obj->vers = contents[2].word;
+  memcpy(obj->d, contents[3].ptr, obj->len);
+  return obj;
+}
+
+
+int
+path_component_encode (struct path_component_s * obj, void * space, int spacelen)
+{
+  layoutvalue_t contents[5] = { 0, };
+  int retval;
+
+  contents[0].word = obj->typ;
+  contents[1].word = obj->len;
+  contents[2].word = obj->vers;
+  contents[3].ptr = 0;
+  contents[4].word = 4 + obj->len;
+  udf_encode(space, spacelen, udf_path_component, contents);
+  memcpy(((uint8_t*)space)+4, obj->d, obj->len);
+  retval = 4 + obj->len;
+
+  return retval;
+}
+
 
 
